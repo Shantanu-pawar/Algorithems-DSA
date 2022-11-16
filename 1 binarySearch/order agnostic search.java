47f1f4a50b1599d6c628a326430e494946ce4a1e
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2022-11-16 10:19:10 +0530

    Update order agnostic search.java

--- 1 binarySearch/order agnostic search.java
+++ 1 binarySearch/order agnostic search.java
@@ -1,19 +1,26 @@
 public class Main
 {
 	public static void main(String[] args) {
-            int [] arr =  {15, 14, 13, 12, 11, 10, 9, 8, 5, 4, 3, 2, 1};  // {8};
-            int target = 8;
+//             int [] arr =  {15, 14, 13, 12, 11, 10, 9, 8, 5, 4, 3, 2, 1};  // {8};
+	
+            int [] arr =  {2, 3};
+            int target = 3;
             int n = arr.length;
             
-            if(arr[0] < arr[n-1]) {
+            // corner case : if arr is null 
+            if(arr.length == 0) {
+                System.out.println("ARr is null"); 
+            }
+            
+            else if(arr[0] < arr[n-1]) {
                 System.out.println(ascending(arr, target));
             }
+		
             else System.out.println(decending(arr, target));
-            
-            
 	}
+	
 	static int ascending (int [] arr, int target) {
-	   int start  =0;
+	   int start = 0;
 	    int end = arr.length-1;
 	    
 	    while(start <= end) {
