8a37a2b98447cbc1d323c28bb5ad1717f715f4ed
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2022-11-19 18:26:13 +0530

    Update 2D arr sort all rows.java

--- 2D array problms/2D arr sort all rows.java
+++ 2D array problms/2D arr sort all rows.java
@@ -1,11 +1,19 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+/*
+matrix : 3 3 
 
-*******************************************************************************/
+3 2 1 
+6 5 4 
+9 8 7 
+
+output-
+
+1 2 3 
+4 5 6 
+7 8 9 
+
+*/
+
 import java.util.*;
 
 public class Main
