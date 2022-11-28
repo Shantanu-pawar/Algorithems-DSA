9e6fb3cc594a81f87f58b8c0ff4e693aa01760e4
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2022-11-28 11:13:35 +0530

    save

--- 2 Stacks & queus/Stacks all basic functions and implimentation.java
+++ 2 Stacks & queus/Stacks all basic functions and implimentation.java
@@ -1,26 +1,28 @@
-
-
 import java.util.*;
-
 public class Main
 {
 	public static void main(String[] args) {
-            
-            Stack<Integer> st = new Stack<>();
-            st.push(45);
-            st.push(23);
-            st.push(10);
-            st.push(12);
-            st.push(20);
-            st.push(22232);
-
-            st.pop();  // delete last added element 
-            System.out.println(st.peek());   // just shows the last element which add in Stack
-            
-            System.out.println(st.size() + " : size of stack"); // represents size of Stack
-            
-            System.out.println("check if element is empty : " + st.empty());
-            
+		Stack<Integer> st = new Stack<>();
+		
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);
+		st.push(6);
+		st.push(756);
+		st.push(8);
+		
+		System.out.println("print your Stack : " + "" + st);
+		
+		System.out.println( '\n' +"delete element and then print it -> " + "" + st.pop());
+		System.out.println(st);
+		
+		System.out.println('\n' +" denotes only top element -> "+ st.peek());
+		
+		System.out.println('\n' +"now check if stack is empty -> " + st.empty());
+		System.out.println('\n' +"reffer the size of stack -> " + st.size());
+		
+		
 	}
 }
-
