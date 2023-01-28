41a0544eeb0baa70c4ad8673037ac08a8c82f463
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-01-28 10:55:37 +0530

    Update Hashmaps implimentation.java

--- 3 HashMaps/Hashmaps implimentation.java
+++ 3 HashMaps/Hashmaps implimentation.java
@@ -2,9 +2,7 @@ import java.util.*;
  
 public class Main {
     public static void main(String[] args) throws Throwable {
-// hashing
             Hashmap<String, Integer> hs = new Hashmap<>();
-            
             // add the elements we use .put
             hs.put("shantanu", 001);
             hs.put("dadu", 34);
@@ -18,7 +16,33 @@ public class Main {
             
             // NOtes : in arrays you have only specific indexes 
             // but in hashmaps you have whatever type of indexes just relate it
-            
-            
     }
+}
+
+//  how to print keyset in hashmaps 
+
+import java.util.*;
+class Main{
+	public static void main(String []args) {
+
+		String str = "asdf";
+		String aa = "fdsa";
+		
+	HashMap<Character, Integer> map = new HashMap<>();
+	
+	for(int i=0; i<str.length(); i++) {
+	    char ch = str.charAt(i);
+	    char ch2 = aa.charAt(i);
+	    
+	    if(map.containsKey(ch)) {
+	        map.put(ch, map.get(ch) + 1);
+	    }
+	    else map.put(ch, 1);	    
+	}
+
+//   printing the storing element's using keyset
+    for(Character a : map.keySet())
+    System.out.print(a +" ");
+
+	}
 }
