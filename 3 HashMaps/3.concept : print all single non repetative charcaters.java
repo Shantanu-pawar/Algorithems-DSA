c4ae6613fdb133dc3b682ff053ceff971f63681a
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-02-03 13:04:19 +0530

    Update 3.concept : print all single non repetative charcaters.java

--- 3 HashMaps/3.concept : print all single non repetative charcaters.java
+++ 3 HashMaps/3.concept : print all single non repetative charcaters.java
@@ -41,7 +41,34 @@ class Main{
 // 		    if( map.get(a) == 1) 
 // 		    System.out.print(a + " ");
 // 		}
-		
 	}
 }
+
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		
+// 		arrr  = {2, 6, 4, 2};
+// 		output " 6 4 "
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+	
+		for(int i = 0; i < n; i++)
+		    arr[i] = sc.nextInt();
+		    
+		int fre[] = new int[1000];
+		for(int i = 0; i < n; i++)
+		    fre[arr[i]]++;
+		  
+		for(int i = 0; i < n; i++)
+		    if(fre[arr[i]] == 1) 
+		        System.out.print(arr[i] + " ");
+	}
+}
+
+
+
+
 
