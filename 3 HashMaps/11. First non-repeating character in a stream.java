10112c69f06179289e8634b102ddf0e2a8763594
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-02-12 15:41:42 +0530

    Update 11. First non-repeating character in a stream.java

--- 3 HashMaps/11. First non-repeating character in a stream.java
+++ 3 HashMaps/11. First non-repeating character in a stream.java
@@ -2,10 +2,40 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Main
-{
-    public static void main(String[] args) throws IOException
-    {
+	// optimal approach using queue's 
+class Solution{
+    public String FirstNonRepeating(String st){
+		String ans = "";
+		
+		// case 1:create an freq arr for storing freq
+		int [] freq = new int[26];
+		Queue <Character > q = new LinkedList<>();
+
+		// case2 : keep popping until we get our freq as 1 and print
+		// if all element's poped means print # cause no element is non-repeating
+		for(int i=0; i<st.length(); i++ ){
+			char ch = st.charAt(i);
+
+// so add freq and char into our Queue
+			freq[ch - 'a']++;
+			q.add(ch);
+
+// case 3: pop the queue until the front char freq is one 
+			while(q.size() > 0  && freq[q.peek() - 'a'] > 1)  q.poll();
+
+// case 4 : if queue become's emplty then you dont have non repeating char's 
+			if(q.size() == 0) ans += "#";
+			else ans += q.peek(); // here we just store the last character whose freq is 1
+		}
+		
+		return ans;
+	}
+}
+
+
+class Main{
+    public static void main(String[] args) throws IOException{
+	    
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String A = br.readLine().trim();
         Solution ob = new Solution();
