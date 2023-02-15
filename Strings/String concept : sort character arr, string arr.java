d9066a378f1be2b1194ce225eb52434ff4464a35
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-02-15 16:45:12 +0530

    Update String concept : sort character arr, string arr.java

--- Strings/String concept : sort character arr, string arr.java
+++ Strings/String concept : sort character arr, string arr.java
@@ -1,36 +1,31 @@
 import java.util.*;
-class Main{
-	public static void main(String []args) {
-        
-        
-        //1. Sort the character array using collection libraries 
-        
-        String str = "zalkfjd";
-        char []arr = str.toCharArray();
-        
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-	
-	
-	   // if we have to split the line and store it into array format then
-	    String str2 = "apple orange banana era";
-	   // we just split the line here
-	   
-        String[] words = str2.split(" ");
-        System.out.println(Arrays.toString(words));
-	    
-	    
-	    
-	   // 2. sort the string input's 
-	   Scanner in = new Scanner(System.in);
-	   int n  = in.nextInt(); 
-	   String []ans1 = new String[n];
-	   
-	   for(int i=0; i<n; i++) {
-	       ans1[i] = in.next();
-	   }
 
-	   Arrays.sort(ans1);
-	   System.out.println(Arrays.toString(ans1));
+public class Main{
+	public static void main(String[] args) {
+		  
+		  // method for sorting the string : 
+		  
+		  String str = "zalkaua";
+		  char [] arr = new char[str.length()];
+		  
+		  for(int i=0; i<str.length(); i++ ) {
+		      arr[i] = str.charAt(i);
+		  }
+		  
+		  Arrays.sort(arr);
+		  System.out.println("now just print using new string : ");
+		  System.out.print(new String(arr));
+
+		  
+		  
+		  // method : for sorting the string Array
+		  
+		  
+		  String a2 = "boy aunion catu";
+		  String [] word = a2.split(" ");
+		  
+		  Arrays.sort(word);
+		              System.out.println();		  
+		  System.out.println(Arrays.toString(word));
 	}
 }
