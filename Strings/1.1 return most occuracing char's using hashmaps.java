a12b5d8fe08f3d8c4f774aca7079c727d6a79f53
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-03-23 21:19:23 +0530

    Update 1.1 return most occuracing char's using hashmaps.java

--- Strings/1.1 return most occuracing char's using hashmaps.java
+++ Strings/1.1 return most occuracing char's using hashmaps.java
@@ -9,22 +9,14 @@ public class Main {
         
         for(int i=0; i<n; i++) {
             char ch = str.charAt(i);
-            
-// we have to check if our map contains key or not 
-//case1 :if our character is already present in DS, just increase its repeatations by 1
-
             if( map.containsKey(ch) ) map.put(ch, map.get(ch) + 1);
-            
-// case 2: if char is putting first time then just put 
             else  map.put(ch, 1);
-            
         }
 
-// case 3 : now just traverse all unique char and check whose occurance is more        
+// case : now just traverse all unique char and check whose occurance is more        
         int max = 0; 
         char ans = ' ';
-        for(Character ch : map.keySet() ) //it represent's all the set that we stored in HashMap
-        {
+        for(Character ch : map.keySet()) { //it represent's all the set that we stored in HashMap
             if(max < map.get(ch) ) {
                 ans = ch; // store char
                 max = map.get(ch); // store repeatations no
@@ -34,3 +26,34 @@ public class Main {
         System.out.println("most repeated char is : " + ans + "\n and nos of repeatations is : " + max);
     }
 }
+
+
+
+// Approach : using freqency map 
+
+class Main {
+    public static void main(String []args ) {
+        String s = "aaabbbzz"; 
+        // HashMap<Character, Integer> map = new HashMap <>();
+        
+        int []freq = new int[26];
+        for(int i=0; i<s.length(); i++)
+            freq[s.charAt(i) - 'a']++;
+        
+        System.out.println(Arrays.toString(freq));
+        
+        int max = 0;
+        char ans = ' ';
+        for(int i=0; i < s.length() ; i++) {
+            char ch = s.charAt(i);
+            if(freq[ch - 'a'] >= max) {
+                ans = ch;
+                max = freq[ch - 'a'];
+            }
+        }
+        
+        System.out.print(ans);
+    }
+}
+
+
