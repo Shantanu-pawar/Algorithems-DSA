e60c4e2a84d204ec1d9bd92413340afc28a802a7
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2023-03-23 11:55:41 +0530

    update Freq arr approach

--- 3 HashMaps/2.find the repetations of character from string.java
+++ 3 HashMaps/2.find the repetations of character from string.java
@@ -32,3 +32,34 @@ class Main{
 		System.out.print("element not present");
 	}
 }
+
+
+// Approach : using freqency arr 
+public class Main {
+    public static void main(String[] args) {
+        
+       String st = "leetcode";
+       int freq[] = new int[26];
+        char target = 'e';
+        
+        for(int i=0; i<st.length(); i++) {
+            char ch = st.charAt(i);
+            freq[ch - 'a']++;
+        }
+        
+        boolean check = false;
+        for(int i=0; i<st.length(); i++) {
+            char ch = st.charAt(i);
+            if(ch == target)  {
+               check = true; break;
+            }
+        }
+        System.out.println(Arrays.toString(freq));
+        
+        if(check == true ) 
+            System.out.println(freq[target - 'a']);
+        
+        else System.out.println("your element is not present in FreqArr");
+        
+   }
+}
